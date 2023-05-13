9f30cd3ceaa90ae8cb78fd36fe0be5ee5968f718
Matheus Gasperotto <66803192+matheusgasper@users.noreply.github.com>
2023-05-13 17:00:15 -0300

    Update escolher_jogo.py

--- escolher_jogo.py
+++ escolher_jogo.py
@@ -1,11 +1,12 @@
 import forca
 import adivinhacao
+import Jogo_penaltis
 
 print("*********************************")
 print("*******Escolha o seu jogo!*******")
 print("*********************************")
 
-print("(1) Forca (2) Adivinhação")
+print("(1) Forca (2) Adivinhação (3) Penaltis")
 
 jogo = int(input("Qual jogo? "))
 
@@ -15,3 +16,6 @@ if (jogo == 1):
 elif (jogo == 2):
     print("Jogando adivinhação")
     adivinhacao.jogar()
+elif (jogo == 3):
+    print("Jogando Penaltis")
+    Jogo_penaltis.jogar()
